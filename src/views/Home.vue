b1d345358b06cd71c2785ca785f5d3fca1494645
korayguler <koray.guler@hotmail.com>
2020-12-31 23:29:36 +0300

    Home view created, search and list movies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,60 @@
 <template>
   <div class="home">
-    <h1>home</h1>
+    <movie-search @searchTrigger="searchMovie" />
+    <movie-card :movies="searchResult" @favoriteMovieTrigger="favoriteMovie" />
   </div>
 </template>
+
+<script>
+import Axios from 'axios';
+import MovieSearch from '@/components/MovieSearch';
+import MovieCard from '@/components/MovieCard';
+export default {
+  components: {
+    MovieSearch,
+    MovieCard,
+  },
+  data() {
+    return {
+      searchResult: [],
+      omdb: `http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}`,
+    };
+  },
+  methods: {
+    async searchMovie(searchValue) {
+      if (!searchValue) return;
+
+      const response = await Axios.post(`${this.omdb}&s=${searchValue}`);
+      if (response.status == 200) {
+        this.searchResult = response.data.Search.map((m) => ({
+          ...m,
+          favorite: false,
+        }));
+        console.log(response.data.Search);
+      }
+    },
+    async favoriteMovie(movie) {
+      if (!movie) return;
+
+      this.searchResult.map((m) => {
+        if (m.imdbID === movie.imdbID) {
+          m.favorite = true;
+        }
+      });
+
+      const request = await Axios.post(
+        'http://localhost:3000/favorites',
+        movie,
+      );
+    },
+  },
+};
+</script>
+
+<style>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
