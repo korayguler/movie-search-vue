bc691d6825a8bccebf3aa2ada11429e65aafaa93
korayguler <koray.guler@hotmail.com>
2020-12-31 23:26:30 +0300

    MovieSearch and Header components created

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <div>
+      <ul>
+        <li>
+          <router-link to="/" active-class="active">Anasayfa</router-link>
+        </li>
+        <li>
+          <router-link to="/favorites" active-class="active"
+            >Favorilerim</router-link
+          >
+        </li>
+      </ul>
+      <span> Movie App | kablosuzkedi </span>
+    </div>
+  </header>
+</template>
+
+<style>
+header {
+  width: 100%;
+}
+
+header > div {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+}
+</style>
---
+++ src/components/MovieSearch.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    type="text"
+    autofocus
+    placeholder="Film adını yazın ve enter tuşuna basın..."
+    class="search_text"
+    v-model="searchValue"
+    @keypress.enter="searchTrigger"
+  />
+</template>
+<script>
+export default {
+  data() {
+    return {
+      searchValue: null,
+    };
+  },
+  methods: {
+    searchTrigger() {
+      this.$emit('searchTrigger', this.searchValue);
+    },
+  },
+};
+</script>
